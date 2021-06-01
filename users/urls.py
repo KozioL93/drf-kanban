961ee9607fd806623ba0a55effd9b8a7627509a3
KozioL93 <bartek.kozlowski93@gmail.com>
2021-06-01 22:05:48 +0200

    Sending verification email

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.views import MyObtainTokenPairView, RegisterView
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name="token_obtain_pair"),
     path('login/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('register/', RegisterView.as_view(), name='auth_register'),
+
 ]
