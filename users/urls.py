b5eb41412482f90510edea38cf52d166aec34132
KozioL93 <bartek.kozlowski93@gmail.com>
2021-06-07 21:00:21 +0200

    Created user page

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from users.views import MyObtainTokenPairView, RegisterView
-
-
+from users.views import MyObtainTokenPairView, RegisterView, CurrentUserView
 urlpatterns = [
     path('login/', MyObtainTokenPairView.as_view(), name="token_obtain_pair"),
     path('login/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
     path('register/', RegisterView.as_view(), name='auth_register'),
-
+    path('profile/', CurrentUserView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 User = get_user_model()
 from .serializers import RegisterSerializer
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from .serializers import MyTokenObtainPairSerializer
+from .serializers import MyTokenObtainPairSerializer, UserSerializer
 
 
 
@@ -20,3 +22,8 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class CurrentUserView(APIView):
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
